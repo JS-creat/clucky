094e19ecd763e267cec3bf238f2b949470df9500
JosueYS <ysabraham633@gmail.com>
2026-02-12 11:01:55 -0500

    Se corrigio errores de escritura

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Producto; // <--- TE FALTABA ESTA LÍNEA
+use App\Models\Producto;
 
 class HomeController extends Controller
 {
