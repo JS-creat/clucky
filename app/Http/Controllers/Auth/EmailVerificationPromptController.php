453128e1b9f53b15b9f06d952b86b3fc5090d9d6
JosueYS <ysabraham633@gmail.com>
2026-03-16 21:07:32 -0500

    Logre solucionar el problema al momento de recargar la pagina de verificacion de correo lanzaba un error de laravel, la solcion se dio en el archivo de emailverificationPromptController dentro de Auth

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class EmailVerificationPromptController extends Controller
@@ -14,8 +15,13 @@ class EmailVerificationPromptController extends Controller
      */
     public function __invoke(Request $request): RedirectResponse|View
     {
-        return $request->user()->hasVerifiedEmail()
-                    ? redirect()->intended(route('dashboard', absolute: false))
-                    : view('auth.verify-email');
+        if ($request->user()->hasVerifiedEmail()) {
+            if ((int) $request->user()->id_rol === 1) {
+                return redirect()->intended(route('admin.dashboard'));
+            }
+            return redirect()->intended('/');
+        }
+
+        return view('auth.verify-email');
     }
 }
